3b36a4af53913ac5a573b64ed81e83fb7f7bb894
weidil <zlouweidi@gmail.com>
2020-04-14 23:14:53 -0700

    dfs

---
+++ DFS.py
@@ -0,0 +1,26 @@
+tree = {
+    'A': ['B', 'C'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D', 'E'],
+    'D': ['B', 'C', 'E', 'F'],
+    'E': ['C', 'D'],
+    'F': ['D']
+}
+
+
+def dfs(graph, root):
+    stack = []
+    stack.append(root)
+    seen = set()
+    seen.add(root)
+    while len(stack) > 0:
+        vertex = stack.pop()
+        nodes = graph[vertex]
+        for node in nodes:
+            if node not in seen:
+                stack.append(node)
+                seen.add(node)
+        print(vertex)
+
+
+dfs(tree, 'A')
